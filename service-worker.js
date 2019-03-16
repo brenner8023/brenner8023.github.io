b57223539728c6327125442487aa1ee6ed623cd9
brenner8023 <brenner8023@163.com>
2019-03-16 14:31:39 +0800

    'null'

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18666a9ec9d4b195a5175b95479f9468.js"
+  "/precache-manifest.56d83d28630a8953ab56072573bc87e6.js"
 );
 
 workbox.clientsClaim();
